747d6dcc12b84ed223f4fc951ed5ede04d43b4b4
Nathanael Rasoamanana <nathanael.rasoamanana@viseo.com>
2026-03-23 17:54:16 +0100

    gestion du formulaire avec react-hook-form

--- src/ui/EditTask.tsx
+++ src/ui/EditTask.tsx
@@ -3,10 +3,34 @@ import type { TaskProps } from "../types/TaskProps";
 import { TasksContext } from "../context/TasksContext";
 import { Box,Container,TextField } from "@mui/material";
 import Bouton from "../components/Button";
+import {useForm, type SubmitHandler} from 'react-hook-form';
+import { getTodayDate } from "@mui/x-date-pickers/internals";
+
+type FormField = {
+    title:string ; 
+    description:string ;
+    date: string;
+}
 
 export default function EditTask(){  
     // Consommation du context
-    const {setTasks} = useContext(TasksContext);    
+    const {setTasks} = useContext(TasksContext);   
+    
+    const {register, handleSubmit} = useForm<FormField>();
+
+    const onSubmit : SubmitHandler<FormField> = (data)=>{
+        console.log(data);
+                // data.preventDefault();
+        if (newTask.title.trim() ===""|| newTask.date.trim() === "") return;
+        
+        const tasksTable = (prev:TaskProps[]):TaskProps[] => [...prev, newTask];
+        setTasks(tasksTable);
+        console.log("L'objet newTask ajouté à tasks",newTask);
+
+        // réinitialisation input 
+        setNewTask({id: crypto.randomUUID(),title: "", description:"", date : "", done:false});
+
+    };
 
     const [newTask, setNewTask] = useState<TaskProps>({
         id:crypto.randomUUID(),
@@ -16,17 +40,17 @@ export default function EditTask(){
         done:false
     });
 
-    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (newTask.title.trim() ===""|| newTask.date.trim() === "") return;
+    // const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    //     event.preventDefault();
+    //     if (newTask.title.trim() ===""|| newTask.date.trim() === "") return;
         
-        const tasksTable = (prev:TaskProps[]):TaskProps[] => [...prev, newTask];
-        setTasks(tasksTable);
-        console.log("L'objet newTask ajouté à tasks",newTask);
+    //     const tasksTable = (prev:TaskProps[]):TaskProps[] => [...prev, newTask];
+    //     setTasks(tasksTable);
+    //     console.log("L'objet newTask ajouté à tasks",newTask);
 
-        // réinitialisation input 
-        setNewTask({id: crypto.randomUUID(),title: "", description:"", date : "", done:false});
-    };
+    //     // réinitialisation input 
+    //     setNewTask({id: crypto.randomUUID(),title: "", description:"", date : "", done:false});
+    // };
 
     //mise à jour uniquement sur value
     const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -59,10 +83,14 @@ export default function EditTask(){
         >
             <Box
                 component="form"
-                onSubmit={handleSubmit}
+                onSubmit={handleSubmit(onSubmit)}
                 sx={{ display: "flex", flexDirection: "column", gap: 2, width: 300 }}
-                >
+            >
                 <TextField
+                    {...register("title",{
+                        required :true,
+                        minLength:3,
+                    })}
                     label="Ajouter une tâche..."
                     name="title"
                     value={newTask.title}
@@ -71,6 +99,7 @@ export default function EditTask(){
                 />
 
                 <TextField
+                    {...register("description")}
                     label="Ajouter une descrition..."
                     name="description"
                     value={newTask.description}
@@ -78,6 +107,9 @@ export default function EditTask(){
                 />
                 
                 <TextField
+                    {...register("date",{
+                        required:true,
+                    })}
                     type="datetime-local"
                     value={newTask.date}
                     onChange={handleChangeDate}
