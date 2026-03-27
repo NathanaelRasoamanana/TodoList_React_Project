f3289559154f7857672035aabb1e37214943a8f5
Nathanael Rasoamanana <nathanael.rasoamanana@viseo.com>
2026-03-27 14:51:05 +0100

    Creation Layout simple pour la navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,37 @@
-import { useState } from "react";
+import { RouterProvider } from "react-router-dom"
+import { router } from "./router/router";
 import type { TaskProps } from "./types/TaskProps";
-import ListTasks from "./pages/ListTasks";
-import EditTask from "./pages/EditTask";
+import { useState } from "react";
 import { TasksContext } from "./context/TasksContext";
 import { Card } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
 
 export default function App(){
 
   const[tasks, setTasks] = useState<TaskProps[]>([
-    {
-      id: crypto.randomUUID(),
-      title: "First task ", 
-      description:"La première tâche à faire... ",
-      date : "2026-03-25T10:45", 
-      done:false,
-      late:false
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Second task ", 
-      date : "2026-04-25T00:00", 
-      done:false,
-      late:false
-    },
-  ]);
+      {
+        id: crypto.randomUUID(),
+        title: "First task ", 
+        description:"La première tâche à faire... ",
+        date : "2026-03-25T10:45", 
+        done:false,
+        late:false
+      },
+      {
+        id: crypto.randomUUID(),
+        title: "Second task ", 
+        date : "2026-04-25T00:00", 
+        done:false,
+        late:false
+      },
+    ]);
 
-  const valueTasksContext = {
+    const valueTasksContext = {
       tasks : tasks,
       setTasks : setTasks,
     }
-
+    
   return (
     <TasksContext.Provider value={valueTasksContext}>
-      <>
         <Card 
             sx={{
                 display: 'grid',
@@ -41,15 +39,10 @@ export default function App(){
                 height: 'flex',
                 m:25,
                 p:2,
-          }}
+            }}
         >
-          <Routes>
-              <Route path="/" element={<ListTasks />} />
-              <Route path="/form" element={<EditTask />} />
-          </Routes>
-
+            <RouterProvider router= {router}/>  
         </Card>
-      </>
     </TasksContext.Provider>
-  )
+  ) 
 }
--- src/components/NavLayout.tsx
+++ src/components/NavLayout.tsx
@@ -1,15 +1,34 @@
+import { Box } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
+import Bouton from "./Button";
 
 export default function NavLayout(){
     return(
         <>
             <header>
                 <nav>
-                    <NavLink to="/list">Liste</NavLink> 
-                    <NavLink to="/form">Formulaire</NavLink> 
+                    <Box sx={{p:1, gap:1, display: "flex", justifyContent: 'flex-start'}} >
+                        <NavLink to="/list">
+                            <Bouton
+                                type="button"
+                                buttonText ="Liste"
+                                txtColor="white"
+                                bgcolor="black"
+                            />
+                        </NavLink> 
+
+                        <NavLink to="/form">
+                            <Bouton 
+                                type="button"
+                                buttonText ="Formulaire"
+                                txtColor="white"
+                                bgcolor="black"
+                            /> 
+                        </NavLink>
+                    </Box>
                 </nav>
             </header>
             <div><Outlet/></div>
         </>
     )
-}
+}        
